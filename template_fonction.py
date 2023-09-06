be3943c6ff374df47270d637ae27836eb21a2be2
Carole Belliardo, PhD <belliardo.carole@hotmail.fr>
2023-09-07 00:38:44 +0200

    Create template_fonction.py

---
+++ template_fonction.py
@@ -0,0 +1,11 @@
+def lire_fichier_fasta(nom_fichier):
+    """
+    Lit un fichier FASTA et renvoie les séquences en tant que dictionnaire.
+    
+    Args:
+        nom_fichier (str): Le nom du fichier FASTA à lire.
+        
+    Returns:
+        dict: Un dictionnaire avec les noms de séquence comme clés et les séquences comme valeurs.
+    """
+    #  code ici
